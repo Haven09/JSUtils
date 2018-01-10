3bdfb34f0ec7740d88a769372a5a05379b52746d
haven <havenhu945@gmail.com>
2018-01-10 18:25:46 +0800

    update .gitignore

---
+++ src/formatCurrentTime.js
@@ -0,0 +1,34 @@
+/**
+ * description: 将时间格式化显示
+ * Created Date: 2018-01-10 4:19:23 pm
+ * Author: Haven
+ * -----
+ * Last Modified:
+ * Modified By:
+ * -----
+ */
+function formatCurrentTime(time) {
+  if (!value) {
+    return '';
+  }
+  var t = new Date(value),
+    y = t.getFullYear(),
+    M = t.getMonth() + 1,
+    d = t.getDate(),
+    h = t.getHours(),
+    m = t.getMinutes(),
+    s = t.getSeconds();
+
+  function format(val) {
+    return val < 10 ? '0' + val : val + ''
+  }
+  M = format(M)
+  d = format(d)
+  h = format(h)
+  m = format(m)
+  s = format(s)
+
+  return `${y}-${M}-${d} ${h}:${m}:${s}`
+}
+
+module.exports = formatCurrentTime
---
+++ src/index.js
@@ -0,0 +1,14 @@
+/**
+ * description: webpack打包入口
+ * Created Date: 2018-01-10 5:36:08 pm
+ * Author: Haven
+ * -----
+ * Last Modified:
+ * Modified By:
+ * -----
+ */
+const formatCurrentTime = require('./formatCurrentTime');
+
+module.exports = {
+  formatCurrentTime
+}
